b3299e82444b366abe854857edc5afcf1ff2d12f
ts614 <thesoldier614@gmail.com>
2023-10-14 11:59:15 +0800

    initial commit

---
+++ ASI.Basecode.Data/ELASIContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ASI.Basecode.Data.Models;
+
+namespace ASI.Basecode.Data
+{
+    public partial class ELASIContext : DbContext
+    {
+        public ELASIContext()
+        {
+        }
+
+        public ELASIContext(DbContextOptions<ELASIContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
